9d461ac70a9e48795fe31713b3eda1fe3ebc1b50
JohnA28 <John.Alban41@gmail.com>
2023-03-29 18:44:31 -0400

    Initial Commit

---
+++ Slots/SlotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotsApp.swift
+//  Slots
+//
+//  Created by John Alban on 3/29/23.
+//
+
+import SwiftUI
+
+@main
+struct SlotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
